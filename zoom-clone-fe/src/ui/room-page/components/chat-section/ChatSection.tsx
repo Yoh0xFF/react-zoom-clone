0d19513d0f5de2b33228dfeeb95e214957a26bbc
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-03-13 16:53:31 +0400

    Start working on the chat UI.

--- zoom-clone-fe/src/ui/room-page/components/chat-section/ChatSection.tsx
+++ zoom-clone-fe/src/ui/room-page/components/chat-section/ChatSection.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './ChatSection.css';
+import ChatLabel from './components/ChatLabel';
 
 export interface ChatSectionProps {}
 
 export default function ChatSection(props: ChatSectionProps): JSX.Element {
-  return <div className='chat_section_container'>Chat section</div>;
+  return (
+    <div className='chat_section_container'>
+      <ChatLabel />
+    </div>
+  );
 }
---
+++ zoom-clone-fe/src/ui/room-page/components/chat-section/components/ChatLabel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ChatLabel.css';
+
+export default function ChatLabel(): JSX.Element {
+  return (
+    <div className='chat_label_container'>
+      <p className='chat_label_paragraph'>CHAT</p>
+    </div>
+  );
+}
